0ad53f52d8de43b38e545c8c7d0de488461bb648
recursivesauron <recursivesauron@gmail.com>
2018-09-22 05:21:17 -0400

    base index.js added. package.json file added

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('NDg1OTM3MTYwNjg0MzA2NDMz.DoeY8g.7Gb0Nx0MI-o-ISysYsMxvx9pCbs');
+
