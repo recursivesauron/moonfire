1f1e8d8c76eb82026fec6278bc5040db78c3cad2
recursivesauron <recursivesauron@gmail.com>
2018-09-23 11:47:27 -0400

    added pretter and eslint config files based on Izzac's project

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    singleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    semi: true,
+    useTabs: false,
+    // prettier-eslint doesn't currently support
+    // inferring these two (Pull Requests welcome):
+    parser: 'babylon',
+    jsxBracketSameLine: false,
+  };
